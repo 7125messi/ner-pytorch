0d8567f4ee4dda447e64793c9a879fb944463bc4
7125messi <zydeve@163.com>
2019-07-09 22:48:15 +0800

    boson数据集测试

--- data/boson/data_util.py
+++ data/boson/data_util.py
@@ -1,35 +1,38 @@
+#! /usr/bin/python
+#coding=utf-8
 import codecs
 import pandas as pd
 import numpy as np
 import re
 
 def data2pkl():
+    """
+    将wordtagsplit转换成pkl文件：
+    """
     datas = list()
     labels = list()
     linedata=list()
     linelabel=list()
     tags = set()
 
-    input_data = codecs.open('./wordtagsplit.txt','r','utf-8')
-    for line in input_data.readlines():
-        line = line.split()
-        linedata=[]
-        linelabel=[]
-        numNotO=0
-        for word in line:
-            word = word.split('/')
-            linedata.append(word[0])
-            linelabel.append(word[1])
-            tags.add(word[1])
-            if word[1]!='O':
-                numNotO+=1
-        if numNotO!=0:
-            datas.append(linedata)
-            labels.append(linelabel)
-
-    input_data.close()
-    print(len(datas),tags)
-    print(len(labels))
+    with codecs.open('./wordtagsplit.txt','r','utf-8') as input_data:
+        for line in input_data.readlines():
+            line = line.split()
+            linedata=[]
+            linelabel=[]
+            numNotO=0
+            for word in line:
+                word = word.split('/')
+                linedata.append(word[0])
+                linelabel.append(word[1])
+                tags.add(word[1])
+                if word[1]!='O':
+                    numNotO += 1
+            if numNotO != 0:
+                datas.append(linedata)
+                labels.append(linelabel)
+        print(len(datas),tags)
+        print(len(labels))
     
     # itertools.chain是平坦任何嵌套的可迭代级别的最佳解决方案，它具有很高的效率
     import collections
@@ -45,14 +48,16 @@ def data2pkl():
 
     sr_allwords = pd.Series(all_words)
     sr_allwords = sr_allwords.value_counts()
+
     set_words = sr_allwords.index
     set_ids = range(1, len(set_words)+1)
 
-
     tags = [i for i in tags]
     tag_ids = range(len(tags))
+
     word2id = pd.Series(set_ids, index=set_words)
     id2word = pd.Series(set_words, index=set_ids)
+
     tag2id = pd.Series(tag_ids, index=tags)
     id2tag = pd.Series(tags, index=tag_ids)
 
@@ -61,16 +66,16 @@ def data2pkl():
     max_len = 60
     def X_padding(words):
         ids = list(word2id[words])
-        if len(ids) >= max_len:  
+        if len(ids) >= max_len:
             return ids[:max_len]
-        ids.extend([0]*(max_len-len(ids))) 
+        ids.extend([0]*(max_len-len(ids)))
         return ids
 
     def y_padding(tags):
         ids = list(tag2id[tags])
-        if len(ids) >= max_len: 
+        if len(ids) >= max_len:
             return ids[:max_len]
-        ids.extend([0]*(max_len-len(ids))) 
+        ids.extend([0]*(max_len-len(ids)))
         return ids
     df_data = pd.DataFrame({'words': datas, 'tags': labels}, index=range(len(datas)))
     df_data['x'] = df_data['words'].apply(X_padding)
@@ -82,7 +87,7 @@ def data2pkl():
     x_train,x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=43)
     x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train,  test_size=0.2, random_state=43)
 
-    
+
     import pickle
     import os
     with open('../Bosondata.pkl', 'wb') as outp:
@@ -133,15 +138,19 @@ def origin2tag():
 
 
 def tagsplit():
+    """
+    根据分隔符：[，。！？、‘’“”（）]/[O]，将每行分割成若干个部分
+    """
     with open('./wordtag.txt','rb') as inp:
         texts = inp.read().decode('utf-8')
-    sentences = re.split('[，。！？、‘’“”（）]/[O]', texts)
-    output_data = codecs.open('./wordtagsplit.txt','w','utf-8')
-    for sentence in sentences:
-	    if sentence != " ":
-		    output_data.write(sentence.strip()+'\n')
-    output_data.close()
-
+        # print(texts)
+    sentences = re.split('[，。！？、‘’“”（）]/[O]',texts)
+    # print(sentences[0])
+    # print(len(sentences))
+    with codecs.open('./wordtagsplit.txt','w','utf-8') as output_data:
+        for sentence in sentences:
+            if sentence != " ":
+                output_data.write(sentence.strip()+'\n')
 
 origin2tag()
 tagsplit()
